97e3731a6638c79cf1c5a33547781a8851992aeb
ivaaaaaaaaaaaa <ivanrazvivan@gmail.com>
2025-02-16 20:09:22 +0300

    Update Marketplace.sol
    добавлен NatSpec формат комментариев

--- Marketplace.sol
+++ Marketplace.sol
@@ -1,43 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // импортируем контракт openzeppelin ERC721 для того что бы не строить велосепед заново
-import "@openzeppelin/contracts/utils/Strings.sol"; // импортируем библиотеку openzeppelin Strings для удобной конкатенации строк и чисел (это нам поможет в создании URI)
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; 
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 
-contract Marketpalce is ERC721 { // импортируем
-  using Strings for uint256; // импортируем
+contract Marketpalce is ERC721 { 
+  using Strings for uint256; 
 
-  uint256 public tokenIds; // id наших токенов, для того что бы не было повторок
+  /**
+   * @dev createNFT => tokenIds++    
+   */
+  uint256 public tokenIds;
 
+  /**
+   * @dev tokenURIs[tokenID] = tokenURI 
+   */
   mapping ( uint256 tokenID => string tokenURI ) private tokenURIs; // хранение URI для каждого NFT
+
+  /**
+   * @dev prices[tokenID] = price
+   */
   mapping ( uint256 tokenID => uint256 price ) private prices;
 
 
+  /**
+   * 
+   * @param tokenId айди токена
+   * @param tokenURI URI ссылка
+   * @param price цена на токен
+   */
   event CreateNFT(uint256 indexed tokenId, string indexed  tokenURI, uint256 indexed price); // событие на создание NFT
 
+  
 
   error ZeroPrice(uint price); // пользователь указал нулевую цену при создании токена
   error NotEnoughMoney(uint amount); // пользователь отправил меньше эфира, чем было указанно  
   error FailedTransaction(); // передача денег юзеру закончилась ошибкой
-  
   error TokenNotExsisit(uint tokenId); // токена, по такому id - не существует 
-//   error TokenAlredyExsisit(uint tokenId); // токен уже существует
   
+  /**
+   * @notice владелец контракта
+   */
+  address public owner; 
 
-  address public owner; // владелец контаркта
-
+  
+  /**
+   * 
+   * @param _tokenId токен айди
+   */
   modifier tokenNotExsist (uint _tokenId) { // проверка на то что токен вообще существует
     if (ownerOf(_tokenId) == address(0)) revert TokenNotExsisit(_tokenId);
     _;
   }
 
-  
+  /**
+   * @dev 
+   * @param _tokenId токен айди
+   * @param _tokenURI URI ссылка 
+   */
   function _setTokenURI(uint _tokenId, string calldata _tokenURI) internal virtual tokenNotExsist(_tokenId){ // устанавливаем URI для NFT. Функция принимает id токена и сам URI.
   tokenURIs[_tokenId] = _tokenURI; 
   }
 
-  function tokenURI( // функция для площадки, что бы на фронте считывалась эта функция и получала данные об URI определенного токена
+
+  /**
+   * @dev функция для площадки, что бы на фронте считывалась эта функция и получала данные об URI определенного токена
+   * @param _tokenId токен айди
+   */
+  function tokenURI(
     uint256 _tokenId
   ) public view virtual override tokenNotExsist(_tokenId) returns(string memory) {
 
@@ -53,6 +84,12 @@ contract Marketpalce is ERC721 { // импортируем
   }
 
 
+
+  /**
+   * 
+   * @param name имя токена 
+   * @param symbol символ токена
+   */
   constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     owner = msg.sender;
     _safeMint(address(this), tokenIds); // минтим одно, тестовое NFT
@@ -60,6 +97,11 @@ contract Marketpalce is ERC721 { // импортируем
   } 
 
   
+  /**
+   * 
+   * @param _tokenURI URI ссылка 
+   * @param _price цена токена
+   */
   function createNFT(string calldata _tokenURI, uint256 _price) public payable { 
    uint256 newTokenId = tokenIds;
    require(msg.value >= _price, NotEnoughMoney(msg.value));
@@ -75,15 +117,16 @@ contract Marketpalce is ERC721 { // импортируем
    
    emit CreateNFT(newTokenId, _tokenURI, _price);
    
-   // передача сдачи выполняется в последнюю очередь, для защиты от reetrancyAttack
+   /**
+    * @notice передача средств происходит в последний момент во избежании reentrancy attack
+    * @notice если хакер захочет во второй раз зайти в эту функцию, то tokenIds увеличться на 1
+    */
    
    if(refund  > 0){
     (bool success, ) = msg.sender.call{value : refund}("");
     require(success, FailedTransaction());
    }
   }
-
-  // вскоре везде будут поставленны комментарии в стиле "Документационные комментарии"
 
 
 }
